21d777ad40812b771f1e28f7d151f59288cf8a30
syed wakil <syedawakil@gmail.com>
2020-09-13 20:14:26 +1000

    added test cases for api service

--- src/app/services/bus-api.service.spec.ts
+++ src/app/services/bus-api.service.spec.ts
@@ -6,6 +6,7 @@ import { environment } from '../../environments/environment';
 import { HttpTestingController } from '@angular/common/http/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LoggerTestingModule } from 'ngx-logger/testing';
+import { BusOperator } from '../model/bus-operator';
 
 describe('BusApiService', () => {
   let service: BusApiService;
@@ -26,5 +27,57 @@ describe('BusApiService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('the data array returned from JSON server is exppected to have elements', (done: DoneFn) => {
+    service.getBusListing().subscribe((data) => {
+      expect(data.length).toBeGreaterThan(0);
+      done();
+    });
+  });
+
+  it('the update service should return updated JSON object from JSON server ', (done: DoneFn) => {
+    const busOwner: any = {
+      id: '27c72c67-960d-476d-b39f-b44e5d9244f6',
+      organisation: 'Sydney Buses',
+      date: '25/09/2015',
+      notes: '',
+      busData: [
+        {
+          busId: '42612',
+          routeVariant: '891 2 1',
+          deviationFromTimetable: 77,
+        },
+        {
+          busId: '29016',
+          routeVariant: '400 1 1',
+          deviationFromTimetable: 340,
+        },
+        {
+          busId: '90467',
+          routeVariant: '393 1 1',
+          deviationFromTimetable: 220,
+        },
+        {
+          busId: '88836',
+          routeVariant: 'M20 1 0',
+          deviationFromTimetable: -287,
+        },
+        {
+          busId: '79367',
+          routeVariant: 'L21 2 1',
+          deviationFromTimetable: 347,
+        },
+      ],
+      status: 'down',
+    };
+    service.updateBusItem(busOwner).subscribe((data) => {
+      expect(data).toBeTruthy();
+      done();
+    });
+  });
+
+  afterEach(() => {
+    TestBed.resetTestingModule();
   });
 });
